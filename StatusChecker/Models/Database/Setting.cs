eaebf6cf193a727c80807f2893874e381a1ef32c
Philipp Moser <info@philipp-moser.de>
2020-12-22 12:14:42 +0100

    SC-35: Add new Setting DatabaseClass

---
+++ StatusChecker/Models/Database/Setting.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+
+namespace StatusChecker.Models.Database
+{
+    public class Setting
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+
+    public enum SettingKeys
+    {
+
+    }
+}
