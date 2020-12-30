670dea01940ec431d597a829002b0d28529fb5d7
Philipp Moser <info@philipp-moser.de>
2020-12-31 00:36:14 +0100

    SC-39: Adding all Configurations to Replacing Build-Script

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -9,11 +9,13 @@ APPSETTINGS_FILE=$APPCENTER_SOURCE_DIRECTORY/StatusChecker/appsettings.json
 # Replacing AppSettings.Json
 if [ -e "$APPSETTINGS_FILE" ]
 then
-    echo "Updating configuration in appsettings.json from Solution"
-    sed -i '' 's/"WebRequestUsername": ""/"WebRequestUsername": "usernameTest"/' $APPSETTINGS_FILE
-    sed -i '' 's/"WebRequestPassword": ""/"WebRequestPassword": "test12345"/' $APPSETTINGS_FILE
+    echo "Updating configuration in appsettings.json"
+    sed -i '' 's/"WebRequestUsername": ""/"WebRequestUsername": "'$CONF_WEBREQUESTUSERNAME'"/' $APPSETTINGS_FILE
+    sed -i '' 's/"WebRequestPassword": ""/"WebRequestPassword": "'$CONF_WEBREQUESTPASSWORD'"/' $APPSETTINGS_FILE
 
-    echo "File content:"
-    cat $APPSETTINGS_FILE
+    sed -i '' 's/"AppCenterSecretForms": ""/"AppCenterSecretForms": "'$CONF_APPCENTERSECRETFORMS'"/' $APPSETTINGS_FILE
+    sed -i '' 's/"AppCenterSecretIOS": ""/"AppCenterSecretIOS": "'$CONF_APPCENTERSECRETIOS'"/' $APPSETTINGS_FILE
+
+    sed -i '' 's/"InitialStatusRequestUrl": ""/"InitialStatusRequestUrl": "'$CONF_INITIALSTATUSREQUESTURL'"/' $APPSETTINGS_FILE
 fi
 # ./Replacing AppSettings.Json
