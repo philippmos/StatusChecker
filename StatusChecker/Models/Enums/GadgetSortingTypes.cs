8ec6319170ddb4e43c341edc4c03ab9963ae9f79
Philipp Moser <info@philipp-moser.de>
2020-12-31 18:13:54 +0100

    SC-50: Add and initialize Sorting-Setting

---
+++ StatusChecker/Models/Enums/GadgetSortingTypes.cs
@@ -0,0 +1,10 @@
+﻿namespace StatusChecker.Models.Enums
+{
+    public enum GadgetSortingTypes
+    {
+        ByCreationAsc = 1,
+        ByCreationDesc = 2,
+        ByNameAsc = 3,
+        ByNameDesc = 4,
+    }
+}
