3108ff5e991959c9bd95b3744481d9f600bfd1f7
Philipp Moser <info@philipp-moser.de>
2021-01-07 18:57:51 +0100

    SC-89: Add some Translations

--- StatusChecker/I18N/AppTranslations.Designer.cs
+++ StatusChecker/I18N/AppTranslations.Designer.cs
@@ -61,6 +61,69 @@ namespace StatusChecker.I18N {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Add.
+        /// </summary>
+        internal static string Main_Button_Title_Add {
+            get {
+                return ResourceManager.GetString("Main_Button_Title_Add", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Back.
+        /// </summary>
+        internal static string Main_Button_Title_Back {
+            get {
+                return ResourceManager.GetString("Main_Button_Title_Back", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Cancel.
+        /// </summary>
+        internal static string Main_Button_Title_Cancel {
+            get {
+                return ResourceManager.GetString("Main_Button_Title_Cancel", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Delete.
+        /// </summary>
+        internal static string Main_Button_Title_Delete {
+            get {
+                return ResourceManager.GetString("Main_Button_Title_Delete", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Edit.
+        /// </summary>
+        internal static string Main_Button_Title_Edit {
+            get {
+                return ResourceManager.GetString("Main_Button_Title_Edit", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Save.
+        /// </summary>
+        internal static string Main_Button_Title_Save {
+            get {
+                return ResourceManager.GetString("Main_Button_Title_Save", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Menu.
+        /// </summary>
+        internal static string Main_Navigation_Button {
+            get {
+                return ResourceManager.GetString("Main_Navigation_Button", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Gadgets.
         /// </summary>
         internal static string Main_Navigation_Entries_Gadgets {
@@ -84,6 +147,168 @@ namespace StatusChecker.I18N {
         internal static string Main_Navigation_Entries_Settings {
             get {
                 return ResourceManager.GetString("Main_Navigation_Entries_Settings", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Analytics.
+        /// </summary>
+        internal static string Page_GadgetDetail_H1_GadgetAnalytics {
+            get {
+                return ResourceManager.GetString("Page_GadgetDetail_H1_GadgetAnalytics", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Gadgetdata.
+        /// </summary>
+        internal static string Page_GadgetDetail_H1_GadgetData {
+            get {
+                return ResourceManager.GetString("Page_GadgetDetail_H1_GadgetData", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Gadgetstatus.
+        /// </summary>
+        internal static string Page_GadgetDetail_H1_GadgetStatus {
+            get {
+                return ResourceManager.GetString("Page_GadgetDetail_H1_GadgetStatus", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Amount of Entries:.
+        /// </summary>
+        internal static string Page_GadgetDetail_Label_Analytics_AmountEntries {
+            get {
+                return ResourceManager.GetString("Page_GadgetDetail_Label_Analytics_AmountEntries", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Average Temperature:.
+        /// </summary>
+        internal static string Page_GadgetDetail_Label_Analytics_AverageTemperature {
+            get {
+                return ResourceManager.GetString("Page_GadgetDetail_Label_Analytics_AverageTemperature", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Temperature Max:.
+        /// </summary>
+        internal static string Page_GadgetDetail_Label_Analytics_MaxTemperature {
+            get {
+                return ResourceManager.GetString("Page_GadgetDetail_Label_Analytics_MaxTemperature", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Temperature Min:.
+        /// </summary>
+        internal static string Page_GadgetDetail_Label_Analytics_MinTemperature {
+            get {
+                return ResourceManager.GetString("Page_GadgetDetail_Label_Analytics_MinTemperature", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Description:.
+        /// </summary>
+        internal static string Page_GadgetDetail_Label_Gadget_Description {
+            get {
+                return ResourceManager.GetString("Page_GadgetDetail_Label_Gadget_Description", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Device ID:.
+        /// </summary>
+        internal static string Page_GadgetDetail_Label_Gadget_DeviceId {
+            get {
+                return ResourceManager.GetString("Page_GadgetDetail_Label_Gadget_DeviceId", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to IP Address:.
+        /// </summary>
+        internal static string Page_GadgetDetail_Label_Gadget_IpAddress {
+            get {
+                return ResourceManager.GetString("Page_GadgetDetail_Label_Gadget_IpAddress", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Location:.
+        /// </summary>
+        internal static string Page_GadgetDetail_Label_Gadget_Location {
+            get {
+                return ResourceManager.GetString("Page_GadgetDetail_Label_Gadget_Location", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Gadgetname:.
+        /// </summary>
+        internal static string Page_GadgetDetail_Label_Gadget_Name {
+            get {
+                return ResourceManager.GetString("Page_GadgetDetail_Label_Gadget_Name", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Temperature:.
+        /// </summary>
+        internal static string Page_GadgetDetail_Label_Status_Temperature {
+            get {
+                return ResourceManager.GetString("Page_GadgetDetail_Label_Status_Temperature", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Temperaturestatus:.
+        /// </summary>
+        internal static string Page_GadgetDetail_Label_Status_TemperatureStatus {
+            get {
+                return ResourceManager.GetString("Page_GadgetDetail_Label_Status_TemperatureStatus", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Voltage:.
+        /// </summary>
+        internal static string Page_GadgetDetail_Label_Status_Voltage {
+            get {
+                return ResourceManager.GetString("Page_GadgetDetail_Label_Status_Voltage", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Edit.
+        /// </summary>
+        internal static string Page_Title_EditGadget {
+            get {
+                return ResourceManager.GetString("Page_Title_EditGadget", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Overview.
+        /// </summary>
+        internal static string Page_Title_GadgetOverview {
+            get {
+                return ResourceManager.GetString("Page_Title_GadgetOverview", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to New Gadget.
+        /// </summary>
+        internal static string Page_Title_NewGadget {
+            get {
+                return ResourceManager.GetString("Page_Title_NewGadget", resourceCulture);
             }
         }
     }
