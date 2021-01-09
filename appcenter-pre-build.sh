8dcc539ee45f77f4c64f4fc5bd67c5408051ec71
Philipp Moser <info@philipp-moser.de>
2021-01-09 11:02:57 +0100

    Update PreBuild Script to define AppVersionNumber via Txt File

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -4,6 +4,7 @@
 # Variables
 APPSETTINGS_FILE=$APPCENTER_SOURCE_DIRECTORY/StatusChecker/appsettings.json
 PLIST_FILE=$APPCENTER_SOURCE_DIRECTORY/StatusChecker.iOS/Info.plist
+APPVERSION_FILE=$APPCENTER_SOURCE_DIRECTORY/appversion.txt
 # ./Variables
 
 
@@ -33,6 +34,14 @@ if [ -e "$PLIST_FILE" ]
 then
     echo "Updating configuration in Info.plist"
     sed -i '' 's/$(CFBundleIdentifier)/'$PLIST_CFBUNDLEIDENTIFIER'/' $PLIST_FILE
+
+    if [ -e "$APPVERSION_FILE" ]
+    then
+        read -r appVersionNumber<$APPVERSION_FILE
+        echo "$appVersionNumber"
+
+        sed -i '' 's/$(AppVersionNumber)/'$appVersionNumber'/' $PLIST_FILE
+    fi
 
 fi
 # ./Replacing Info.plist
